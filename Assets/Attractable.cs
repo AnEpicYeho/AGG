1435fb56e794b6c82bf75e3be93ee820870a9007
Yeho <joaquin_gebauer@hotmail.com>
2017-09-11 19:48:38 -0300

    Initial commit

---
+++ Assets/Attractable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Core[] cores = GameObject.FindObjectsOfType <Core>();
+		print (cores.Length);
+		foreach (Core core in cores) {
+			core.AddAttractable (this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Core.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour {
+
+	const float grabitationalConstant = 667.4f;
+	private Rigidbody rigidBody;
+	private List<GameObject> attractables;
+
+
+	public void removeAttractable(GameObject attractable){
+		attractables.Remove (attractable);
+	}
+
+	public List<GameObject> getAttractablesList()
+	{
+		return attractables;
+	}
+
+	public void AddAttractable(GameObject attractable){
+		if(attractables  == null){
+			attractables = new List<GameObject> ();
+		}
+		attractables.Add (attractable);
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		Core[] otherCores = GameObject.FindObjectsOfType<Core> ();
+		foreach(Core otherCore in otherCores){
+			if(otherCore != this){
+				attractables = new List<GameObject> ();
+				attractables = otherCore.getAttractablesList ();
+				print (attractables.Count);
+				break;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(attractables != null){
+		foreach (GameObject attractable in attractables){
+			attract (attractable);
+		}
+		}
+	}
+
+	void attract(GameObject objToAttract){
+		Rigidbody rbToAttract = objToAttract.GetComponent<Rigidbody> ();
+
+		Vector3 direction = this.rigidBody.position - rbToAttract.position;
+		float distance = direction.magnitude;
+
+		if(distance == 0f){
+			return;
+		}
+
+		float forceMagnitude = grabitationalConstant * (this.rigidBody.mass + rbToAttract.mass) / Mathf.Pow (distance, 2);
+
+		Vector3 force = direction.normalized * forceMagnitude;
+
+		rbToAttract.AddForce (force);
+	}
+}
